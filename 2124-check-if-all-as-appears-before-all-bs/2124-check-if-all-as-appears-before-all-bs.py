d378b67e052694b069da6c7da7d6edbfd57429c9
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2022-07-10 10:00:04 -0700

    Time: 57 ms (23.61%), Space: 13.8 MB (54.81%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -3,14 +3,13 @@ class Solution:
         s_dict = {}
         for x in s:
             s_dict[x] = s_dict.get(x, 0) +1
-        print(s_dict)
-        
+        #check if a exist and s start with a
         if "a" in s:
             len_a = s_dict['a']
             if s[:len_a] == ('a' * len_a):
                 return True
+        # if a not exist and if s not empty
         elif "a" not in s and s != '':
-            return True
-            
+            return True 
         return False
             
