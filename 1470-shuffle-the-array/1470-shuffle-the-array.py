ecb7fff7987ceddcc6806c9cd51b66bdb10d7edf
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-01-31 13:01:46 -0800

    Time: 68 ms (51.94%), Space: 14.1 MB (86.90%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,7 +1,10 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         new_list = []
-        for x, y in zip(nums[:n],nums[n:]):
-            new_list.append(x)
-            new_list.append(y)
+        n_list = nums[:n]
+        m_list = nums[n:]
+        print(m_list)
+        for i, j in zip(n_list, m_list):
+            new_list.append(i)
+            new_list.append(j)
         return new_list
