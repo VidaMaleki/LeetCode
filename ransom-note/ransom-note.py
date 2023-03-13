95965ac39d74652fcc25102fc000579c020fc7b3
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-03-12 22:14:38 -0700

    Time: 43 ms, Memory: 14.1 MB - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -1,12 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dict1 = {}
-        dict2 = {}
-        for i in ransomNote:
-            dict1[i] = dict1.get(i, 0) + 1
-            
-        for j in magazine:
-            dict2[j] = dict2.get(j, 0) + 1
+        dict1 = {i: ransomNote.count(i) for i in set(ransomNote)}
+        dict2 = {i: magazine.count(i) for i in set(magazine)}
             
         for k, v in dict1.items():
             if k not in dict2 or dict1[k] > dict2[k]:
