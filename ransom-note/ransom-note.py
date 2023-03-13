8dc5f861958cc747fd759f7344d81bd8c76aab4a
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-03-12 22:07:55 -0700

    Time: 69 ms, Memory: 14.1 MB - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -8,12 +8,7 @@ class Solution:
         for j in magazine:
             dict2[j] = dict2.get(j, 0) + 1
             
-        print(dict1)
-        print(dict2)
         for k, v in dict1.items():
-            print(k, v)
-            if k not in dict2:
-                return False
-            if k in dict2 and dict1[k] > dict2[k]:
+            if k not in dict2 or dict1[k] > dict2[k]:
                 return False
         return True
