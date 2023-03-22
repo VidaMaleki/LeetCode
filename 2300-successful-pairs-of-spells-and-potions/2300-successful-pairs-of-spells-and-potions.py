d1755aedfadd8a7c519d40a7d1de8848fe28a8a6
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-03-22 08:11:49 -0700

    Time: 2712 ms (6.66%), Space: 37.1 MB (53.04%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,19 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        
+        potions.sort()
+        answer = []
+        for i in range(len(spells)):
+            left = 0
+            right = len(potions) -1
+            while left <= right:
+                mid = (left + right) //2
+                # if potions[mid] * spells[i] >= success:
+                if potions[mid] >= (success + spells[i] - 1) // spells[i]:
+                    right = mid -1
+                else:
+                    left = mid +1
+            count = len(potions) - left
+            answer.append(count)
+        return answer
+                
