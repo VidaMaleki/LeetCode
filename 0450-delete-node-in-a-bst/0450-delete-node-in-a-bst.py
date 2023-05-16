d24139335948eb6d2415bfe683a4b19e8db6f346
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-05-15 17:28:11 -0700

    Time: 91 ms (14.08%), Space: 20.7 MB (23.46%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -8,36 +8,27 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return root
-        print("root is >>>" , root)
-        print("left root is >>>", root.left)
-        print("right root is >>>", root.right)
+        
         if key < root.val:
-            print("key < root")
-            root.left = self.deleteNode(root.left, key)
-            
+            root.left = self.deleteNode(root.left, key) 
         elif key > root.val:
-            print("key > root")
-            root.right = self.deleteNode(root.right, key)
-            
+            root.right = self.deleteNode(root.right, key) 
         else:  # Found the node to delete
-            print("key == root")
-            if not root.left:  # Case 2: One child (right)
+            if not root.left:  # key has One child (right)
                 root = root.right
-            elif not root.right:  # Case 2: One child (left)
+            elif not root.right:  #Key has One child (left)
                 root = root.left
-            else:  # Case 3: Two children
+            else:  # key has Two children and we need to choose right one
                 successor = self.findSuccessor(root.right)
                 root.val = successor.val
                 root.right = self.deleteNode(root.right, successor.val)
-        print(root)
         return root
 
 
     def findSuccessor(self,node):
-        print("successor start",node, node.left)
+        # if right node has left node choose that otherwise replace with right
         while node.left:
             node = node.left
-        print("successor end", node)
         return node
 
 
