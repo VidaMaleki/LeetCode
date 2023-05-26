75f7251b20f2bd4abf2f53c87446f9d644e1c70f
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-05-26 14:47:55 -0700

    Time: 92 ms (21.13%), Space: 20.7 MB (19.77%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -10,26 +10,85 @@ class Solution:
             return root
         
         if key < root.val:
-            root.left = self.deleteNode(root.left, key) 
+            root.left = self.deleteNode(root.left, key)
         elif key > root.val:
-            root.right = self.deleteNode(root.right, key) 
-        else:  # Found the node to delete
-            if not root.left:  # key has One child (right)
-                root = root.right
-            elif not root.right:  #Key has One child (left)
-                root = root.left
-            else:  # key has Two children and we need to choose right one
-                successor = self.findSuccessor(root.right)
+            root.right = self.deleteNode(root.right, key)
+        elif key == root.val:
+            if not root.left:
+                 root = root.right
+            elif not root.right:
+                 root = root.left
+            else:
+                successor = self.minimumNode(root.right)
                 root.val = successor.val
-                root.right = self.deleteNode(root.right, successor.val)
+                root.right = self.deleteNode(root.right, root.val)
         return root
-
-
-    def findSuccessor(self,node):
-        # if right node has left node choose that otherwise replace with right
+                
+                
+    def minimumNode(self, node):
         while node.left:
             node = node.left
         return node
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not root:
+#             return root
+        
+#         if key < root.val:
+#             root.left = self.deleteNode(root.left, key) 
+#         elif key > root.val:
+#             root.right = self.deleteNode(root.right, key) 
+#         else:  # Found the node to delete
+#             if not root.left:  # key has One child (right)
+#                 root = root.right
+#             elif not root.right:  #Key has One child (left)
+#                 root = root.left
+#             else:  # key has Two children and we need to choose right one
+#                 successor = self.findSuccessor(root.right)
+#                 root.val = successor.val
+#                 root.right = self.deleteNode(root.right, successor.val)
+#         return root
+
+
+#     def findSuccessor(self,node):
+#         # if right node has left node choose that otherwise replace with right
+#         while node.left:
+#             node = node.left
+#         return node
 
 
 
