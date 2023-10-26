3502655ba99005f47312f450016a5521d855d30d
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2023-10-25 22:59:40 -0700

    Time: 61 ms, Memory: 17.4 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left= 0
-        right = 1
-        while left < len(nums):
-            if nums[left] + nums[right] == target:
-                return [left, right]
-            if right == len(nums) -1:
-                right = left +1
-                left +=1
-            right +=1
-        
+        nums_dict = {}
+        for i, num in enumerate(nums):
+            pair = target - num
+            if pair in nums_dict:
+                return [nums_dict[pair], i]
+            nums_dict[num] = i
+        return []
