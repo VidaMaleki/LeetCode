7be8e700d17bd935252d13e72cf3fb4c8d694e08
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2025-06-11 11:08:08 -0700

    Time: 7 ms (10.49%), Space: 23.1 MB (16.12%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s) -1
-        while left < right:
-            s[left], s[right] = s[right] ,s[left]
-            left +=1
-            right -= 1
-        return s
+        right = 0
+        left = len(s) -1
+        while right < left:
+            s[right], s[left] = s[left], s[right]
+            right +=1
+            left -=1
