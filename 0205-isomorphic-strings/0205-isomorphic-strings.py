a3b19ad720edf27ac4b1a103976d0a278c4109a8
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2025-07-09 13:28:06 -0700

    Time: 6 ms (60.13%), Space: 18.1 MB (36.86%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,13 @@
-from collections import Counter
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        s_map = {}
-        t_map = {}
-
-        for char_s , char_t in zip(s, t):
-
-            if s_map.get(char_s,char_t ) != char_t or t_map.get(char_t,char_s ) != char_s:
+        s_dict, t_dict = {}, {}
+        for s_char, t_char in zip(s, t):
+            if s_dict.get(s_char, t_char) != t_char or t_dict.get(t_char, s_char) != s_char:
                 return False
-            s_map[char_s] = char_t
-            t_map[char_t] = char_s
+            s_dict[s_char] = t_char
+            t_dict[t_char] = s_char
             
         return True
+            
