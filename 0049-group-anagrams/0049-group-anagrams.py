a3f095d49791972cffc18535d53460bddb2e78f0
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2025-07-09 14:17:55 -0700

    Time: 11 ms (82.87%), Space: 21.2 MB (44.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_dict = {}
+        anagram_map = {}
         for word in strs:
-            temp = "".join(sorted(word))
-            if temp not in sorted_dict:
-                sorted_dict[temp] = [word]
-            else:
-                sorted_dict[temp].append(word)
-        return list(sorted_dict.values())
+            temp = tuple(sorted(word))
+            anagram_map.setdefault(temp, []).append(word)
+        return list(anagram_map.values())
