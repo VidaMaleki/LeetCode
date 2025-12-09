93f5ff21b3bdb6e7181d3dacbd8435ce74212435
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2025-12-09 13:16:29 -0800

    Time: 11 ms (73.05%), Space: 17.7 MB (85.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,10 @@
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_map = Counter(s)
-        t_map = Counter(t)
+        if len(s) != len(t):
+            return False
+        s_map, t_map = {}, {}
+        for s_char, t_char in zip(s, t):
+            s_map[s_char] = s_map.get(s_char, 0) +1
+            t_map[t_char] = t_map.get(t_char, 0) +1
+        
         return s_map == t_map
