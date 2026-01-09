9272288cfadc9001466a6f62ca9628f1f84d641f
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2026-01-08 22:32:50 -0800

    Time: 65 ms (12.17%), Space: 19.5 MB (17.42%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -2,12 +2,10 @@ class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         left = 0
         right = n
-        index = 0
         ans = []
-        while index < n:
+        while left < n:
             ans.append(nums[left])
             ans.append(nums[right])
             left+=1
             right+=1
-            index+=1
         return ans
