3c94862e2d94f37f227a8c2ff2a1b7e04d549525
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2026-09-10 21:04:43 -0700

    Time: 11 ms (77.36%), Space: 19.3 MB (94.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,9 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        s_map, t_map = {}, {}
-        for s_char, t_char in zip(s, t):
-            s_map[s_char] = s_map.get(s_char, 0) +1
-            t_map[t_char] = t_map.get(t_char, 0) +1
+        anagram = {}
+        for char in s:
+            anagram[char] = anagram.get(char, 0) +1
         
-        return s_map == t_map
+        for char in t:
+            if char not in anagram or anagram[char]<= 0:
+                return False
+            anagram[char] -= 1
+        return True
