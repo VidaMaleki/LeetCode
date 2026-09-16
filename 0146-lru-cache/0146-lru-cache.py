06655f2c86912a97d9412b73172428eefe496535
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2026-09-16 16:34:19 -0700

    Time: 35 ms (61.94%), Space: 29.4 MB (63.47%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,59 +1,27 @@
-class Node:
-    def __init__(self, k, v):
-        self.k = k
-        self.v = v
-        self.prev = None
-        self.next = None
+from collections import OrderedDict
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}  # key -> Node
-
-        # dummy head/tail
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def _remove(self, node):
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
-
-    def _add_front(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
-
-    def _make_recent(self, node):
-        self._remove(node)
-        self._add_front(node)
+        self.capacity = capacity
+        self.cashe = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.cashe:
             return -1
-        node = self.cache[key]
-        self._make_recent(node)
-        return node.v
+        self.cashe.move_to_end(key)
+        return self.cashe[key]
+    
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
-            node.v = value
-            self._make_recent(node)
-            return
-
-        node = Node(key, value)
-        self.cache[key] = node
-        self._add_front(node)
-
-        if len(self.cache) > self.cap:
-            lru = self.tail.prev
-            self._remove(lru)
-            del self.cache[lru.k]
+        if key in self.cashe:
+            self.cashe[key] = value
+            self.cashe.move_to_end(key)
+        else:
+            if len(self.cashe) >= self.capacity:
+                self.cashe.popitem(last=False)
+            self.cashe[key] = value
+        return self.cashe
         
 
 
