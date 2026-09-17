f944f8ef007f97e39cab772aa472ad2f87e1d652
Vida Maleki <75859694+VidaMaleki@users.noreply.github.com>
2026-09-17 13:13:08 -0700

    Time: 228 ms (90.24%), Space: 21.7 MB (48.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,23 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, columns = len(grid), len(grid[0])
-        seen = set()
-        count = 0
-        def dfs(row, col):
-            if (row < 0 or row >= rows or
-                col < 0 or col >= columns or grid[row][col] == "0" or (row, col) in seen):
-                return
-            seen.add((row, col))
-            dfs(row+1, col)
-            dfs(row-1, col)
-            dfs(row, col+1)
-            dfs(row, col-1)
+        m = len(grid)
+        n = len(grid[0])
         
-        for row in range(rows):
-            for col in range(columns):
-                if grid[row][col] == "1" and (row, col) not in seen:
-                    dfs(row, col)
+        def explore(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != "1":
+                return
+            grid[i][j] = "#"
+            explore(i +1, j)
+            explore(i -1, j)
+            explore(i , j + 1)
+            explore(i, j -1)
+
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
                     count +=1
+                    explore(i, j)
         return count
+
+        
